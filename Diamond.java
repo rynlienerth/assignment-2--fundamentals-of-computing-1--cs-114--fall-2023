3064798de1b5a45a8c4d9e19dddfc217606b852a
rynlienerth <lienerth@hartford.edu>
2023-10-13 14:09:28 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -7,40 +7,43 @@ public class Diamond {
     System.out.println("Enter a number");
     int input = scanner.nextInt();
 
-    if (input % 2 == 0) {
-      for (int i = 0; i < input / 2; i++){
-        for (int spaces = 0; spaces < input / 2 - i; spaces++); {
-        System.out.print(" ");
+    int spaces = input - 1;
+    int astrix = 1;
+
+    if (input % 2 == 0 && input > 0) {
+      for (int i = 0; i < input; i += 2){
+        for (spaces = 0; spaces < (input-i) / 2; spaces++); {
+        System.out.print("  ");
         }
-        for (int astrix = 0; astrix < 2 * i + 1; astrix++) {
-          System.out.print("*");
+        for (astrix = 0; astrix <= i; astrix++) {
+          System.out.print(" *");
         }
         System.out.println();
       }
-      for (int i = input / 2 - 2; i >= 0; i--) {
-        for (int spaces = 0; spaces < input / 2 - i; spaces++){
+      for (int i = 0; i < input; i++) {
+        for (spaces = 0; spaces < (input - i) / 2; spaces++){
           System.out.print("  ");
       }
-      for (int astrix = 0; astrix < 2 * i + 1; astrix++) {
-        System.out.print("* ");
+      for (astrix = 0; astrix <= i; astrix++) {
+        System.out.print(" *");
       }
       System.out.println();
     }
   } else {
       for (int i = 0; i <= input / 2; i++) {
-        for (int spaces = 0; spaces < input / 2 - i; spaces++) {
+        for (spaces = 0; spaces < input / 2 - i; spaces++) {
           System.out.print(" ");
         }
-        for (int astrix = 0; astrix < 2 * i + 1; astrix++) {
+        for (astrix = 0; astrix < 2 * i + 1; astrix++) {
           System.out.print("*");
         }
         System.out.println();
       }
       for (int i = input / 2 - 1; i >= 0; i--){
-        for (int spaces = 0; spaces < input / 2 - i; spaces++) {
+        for (spaces = 0; spaces < input / 2 - i; spaces++) {
           System.out.print(" ");
         }
-        for (int astrix = 0; astrix < 2 * i + 1; astrix++){
+        for (astrix = 0; astrix < 2 * i + 1; astrix++){
           System.out.print("*");
         }
         System.out.println();
