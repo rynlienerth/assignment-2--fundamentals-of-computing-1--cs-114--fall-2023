63a8d9fbab38ac9cda152117be013aff3279ccde
rynlienerth <lienerth@hartford.edu>
2023-10-13 12:58:59 -0400

    Updated Code

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,51 @@
 public class Diamond {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter a number");
+    int input = scanner.nextInt();
+
+    if (input % 2 == 0) {
+      for (int i = 0; i < input; i++){
+        for (int spaces = 0; spaces < input - i - 1; spaces++);
+        System.out.print("  ");
+        }
+        for (int astrix = 0; astrix 2 * i + 1; astrix++) {
+          System.out.print("* ");
+        }
+        System.out.println();
+        }
+      for (int i = input - 2; i >= 0; i--) {
+        for (int spaces = 0; spaces < input - i - 1; spaces++)
+          System.out.print("   ");
+      }
+      for (int astrix = 0; astrix < 2 * i + 1; astrix++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+      }
+      } else {
+       for (int i = 0; i < input / 2; i++) {
+        for (int spaces = 0; spaces < input / 2 - i; spaces++) {
+          System.out.print(" ");
+        }
+        for (int astrix = 0; astrix < 2 * i + 1; astrix++){
+          System.out.print("*");
+        }
+        System.out.println();
+       }
+       for (int i = input / 2 - 2; i >= 0; i--){
+        for (int spaces = 0; spaces < input / 2 - i; spaces++) {
+          System.out.print(" ");
+        }
+        for (int astrix = 0; astrix < 2 * i + 1; astrix++) {
+          System.out.print("*");
+        }
+        System.out.println();
+       }
+       scanner.close();
+      }
+      }
+    }
   }
 }
