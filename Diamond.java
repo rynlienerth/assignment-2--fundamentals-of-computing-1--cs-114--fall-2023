f612120fb93d2a789c96e8c2b1f2cecb4be28535
rynlienerth <lienerth@hartford.edu>
2023-10-13 13:16:48 -0400

    Updated Diamond Code

--- Diamond.java
+++ Diamond.java
@@ -1,40 +1,33 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Enter a number");
+    System.out.println("Enter a number");
     int input = scanner.nextInt();
 
     if (input % 2 == 0) {
-      for (int i = 0; i < input; i++){
-        for (int spaces = 0; spaces < input - i - 1; spaces++);
-        System.out.print("  ");
+      for (int i = 0; i < input / 2; i++){
+        for (int spaces = 0; spaces < input / 2 - i; spaces++); {
+        System.out.print(" ");
         }
-        for (int astrix = 0; astrix 2 * i + 1; astrix++) {
-          System.out.print("* ");
+        for (int astrix = 0; astrix < 2 * i + 1; astrix++) {
+          System.out.print("*");
         }
         System.out.println();
-        }
-      for (int i = input - 2; i >= 0; i--) {
-        for (int spaces = 0; spaces < input - i - 1; spaces++)
-          System.out.print("   ");
+      }
+      for (int i = input / 2 - 2; i >= 0; i--) {
+        for (int spaces = 0; spaces < input / 2 - i; spaces++){
+          System.out.print("  ");
       }
       for (int astrix = 0; astrix < 2 * i + 1; astrix++) {
         System.out.print("* ");
       }
       System.out.println();
-      }
-      } else {
-       for (int i = 0; i < input / 2; i++) {
-        for (int spaces = 0; spaces < input / 2 - i; spaces++) {
-          System.out.print(" ");
-        }
-        for (int astrix = 0; astrix < 2 * i + 1; astrix++){
-          System.out.print("*");
-        }
-        System.out.println();
-       }
-       for (int i = input / 2 - 2; i >= 0; i--){
+    }
+  } else {
+      for (int i = 0; i <= input / 2; i++) {
         for (int spaces = 0; spaces < input / 2 - i; spaces++) {
           System.out.print(" ");
         }
@@ -42,10 +35,17 @@ public class Diamond {
           System.out.print("*");
         }
         System.out.println();
-       }
-       scanner.close();
       }
+      for (int i = input / 2 - 1; i >= 0; i--){
+        for (int spaces = 0; spaces < input / 2 - i; spaces++) {
+          System.out.print(" ");
+        }
+        for (int astrix = 0; astrix < 2 * i + 1; astrix++){
+          System.out.print("*");
+        }
+        System.out.println();
       }
-    }
   }
+  scanner.close();
+}
 }
